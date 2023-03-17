8d7f2290612bc9991c1f10be534bb4d05985b4e6
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-17 23:31:05 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace homework_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Number = ");
+                int n = int.Parse(Console.ReadLine());
+                string counter = "";
+
+                while (n >= 1)
+                {
+                    counter = (n % 2) + counter;
+                    n = n / 2;
+                }
+                Console.Write(counter);
+                Console.WriteLine();
+            }
+        }
+    }
+}
